99021961eaadc2acbe8b9a05c4fe08e4cfc64abb
VICTOR SUMOZA <alonsosumoza0910@gmail.com>
2024-07-25 20:58:38 -0300

    final

--- data/stock.js
+++ data/stock.js
@@ -7,7 +7,7 @@ const productos =
         desc: "Remera 100% Algodon, Estampado JS",
         precio: 1200,
         talle: "L",
-        imagen: '../public/images/JS.jpg',
+        imagen: './public/images/JS.jpg',
         cantidad:1
     },
 
@@ -18,7 +18,7 @@ const productos =
         desc: "Remera 100% Algodon, Estampado AngularJs",
         precio: 1200,
         talle: "L",
-        imagen: '../public/images/AngularJS.jpg',
+        imagen: './public/images/AngularJS.jpg',
         cantidad:1
     },
 
@@ -29,7 +29,7 @@ const productos =
         desc: "Remera 100% Algodon, Estampado Git",
         precio: 1100,
         talle: "S",
-        imagen: '../public/images/git.jpg',
+        imagen: './public/images/git.jpg',
         cantidad:1
     },
 
@@ -40,7 +40,7 @@ const productos =
         desc: "Remera 100% Algodon, Estampado Html",
         precio: 1600,
         talle: "L",
-        imagen: '../public/images/html.jpg',
+        imagen: './public/images/html.jpg',
         cantidad:1
     },
 
@@ -51,7 +51,7 @@ const productos =
         desc: "Remera 100% Algodon, Estampado Node",
         precio: 1200,
         talle: "L",
-        imagen: '../public/images/node.jpg',
+        imagen: './public/images/node.jpg',
         cantidad:1
     },
 
@@ -62,7 +62,7 @@ const productos =
         desc: "Remera 100% Algodon, Estampado Sass",
         precio: 1000,
         talle: "L",
-        imagen: '../public/images/sass.jpg',
+        imagen: 'gi./public/images/sass.jpg',
         cantidad:1
     },
 
